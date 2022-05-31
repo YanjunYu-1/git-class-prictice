1490cdccf23a1cbd56b4699f4f3c1ac5fa59e3f8
YanjunYu-1 <y9y9j8@163.com>
2022-05-31 14:59:11 -0500

    课堂练习，没有全部写完，下一步开始细化

---
+++ 20220531/exercise5/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Circles(){
+
+}
+
+function App() {
+  return (
+  <div className="App">
+    <h1>This works!</h1>
+    <div className="Circles" 
+      style={{
+        background:"orange",
+        width:"300px",
+        height:"300px",
+        borderRadius:"50%",
+      }}>
+    </div>
+  </div>);
+
+}
+
+export default App;
