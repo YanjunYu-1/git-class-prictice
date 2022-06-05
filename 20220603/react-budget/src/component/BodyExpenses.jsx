a0f00a9ede4d9aa4a2c30726dcd18a741216a35a
YanjunYu-1 <y9y9j8@163.com>
2022-06-05 12:12:57 -0500

    暂停此项目，重新做

--- 20220603/react-budget/src/component/BodyExpenses.jsx
+++ 20220603/react-budget/src/component/BodyExpenses.jsx
@@ -12,6 +12,7 @@ const BodyExpenses = () => {
                             <button className="item__delete--btn">
                                 <i className="ion-ios-close-outline"></i>
                             </button>
+                            
                         </div>
                     </div>
                     <div className="item__date">Apr. 21st, 2020</div>
