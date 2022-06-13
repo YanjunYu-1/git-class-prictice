b2648d1f1e15cb9e0b4a2522b8a81749a246ddd3
yanjun yu <84165609+YanjunYu-1@users.noreply.github.com>
2022-06-13 14:02:37 -0500

    优化代码

--- 20220603/heart-movies-by-myself/src/components/MoviesLIst.jsx
+++ 20220603/heart-movies-by-myself/src/components/MoviesLIst.jsx
@@ -1,23 +1,23 @@
 import Movie from "./Movie";
 
-const MoviesList = ({movies,toggleLikeMovie,likedMovies}) => {
+const MoviesList = ({ movies, toggleLikeMovie, likedMovies }) => {
     // console.log(movies)
     return (
         <div className="titleList">
             <div className="title">
                 <h1>Movies</h1>
                 <div className="titles-wrapper">
-                    {movies.map((movie) =>(
-                        <Movie 
-                            key={movie.id} 
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
                             movie={movie}
                             liked={
                                 likedMovies.findIndex(
-                                    (likedMovies)=>movie.id===likedMovies
-                                )===-1
-                                ? false
-                                : true
-                    }
+                                    (likedMovies) => movie.id === likedMovies
+                                ) === -1
+                                    ? false
+                                    : true
+                            }
                             toggleLikeMovie={toggleLikeMovie}
                         />
                     ))}
